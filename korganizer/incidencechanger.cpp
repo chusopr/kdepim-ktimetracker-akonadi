chusopr/kdepim-ktimetracker-akonadi

commit a688c8a83702fe788bc88e44ec7fe0b3ef499de1

Fix potential crash
svn path=/trunk/KDE/kdepim/; revision=949872

korganizer/incidencechanger.cpp
@@ -312,7 +312,7 @@ bool IncidenceChanger::addIncidence( Incidence *incidence, QWidget *parent )
     // widgets sooner or later
     stdcal->setDialogParentWidget( tmpparent );
   }
-  if ( !success ) {
+  if ( !success && stdcal ) {
     // We can have a failure if the user pressed [cancel] in the resource
     // selectdialog, so check the exception.
     ErrorFormat *e = stdcal->exception();

commit 2087cb41e843cf88e033d0ccf4c43082eb15c998

Fixed regression introduced with r949872 on the try to fix a potential crash but ignoring that way the return-value of addIncidence if stdcal was not used. 4.3 backport candidate.
svn path=/branches/work/akonadi-ports/kdepim/; revision=987588

korganizer/incidencechanger.cpp
@@ -310,10 +310,10 @@ bool IncidenceChanger::addIncidence( Incidence *incidence, QWidget *parent )
     // widgets sooner or later
     stdcal->setDialogParentWidget( tmpparent );
   }
-  if ( !success && stdcal ) {
+  if ( !success ) {
     // We can have a failure if the user pressed [cancel] in the resource
     // selectdialog, so check the exception.
-    ErrorFormat *e = stdcal->exception();
+    ErrorFormat *e = stdcal ? stdcal->exception() : 0;
     if ( !e || ( e && ( e->errorCode() != KCal::ErrorFormat::UserCancel ) ) ) {
       KMessageBox::sorry( parent,
                           i18n( "Unable to save %1 \"%2\".",