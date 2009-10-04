chusopr/kdepim-ktimetracker-akonadi

commit e1920dc557d9e81c960c9884806d2a0b6f866fce

Show kcm pages for all the summary plugins, not just the active summary plugins. I think this is less confusing.
The old way went like this: select a summary plugin to be active -> apply/ok -> then configure
Now you can configure and select a summary plugin and then apply/ok

MERGE: none
DO_NOT_BACKPORT:

svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=1030887

kontact/plugins/summary/summaryview_part.cpp
@@ -21,7 +21,6 @@
   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
-
 #include "summaryview_part.h"
 
 #include <kontactinterface/summary.h>
@@ -157,9 +156,12 @@ void SummaryViewPart::updateWidgets()
   QList<KontactInterface::Plugin*>::ConstIterator it = plugins.constBegin();
   for ( ; it != end; ++it ) {
     KontactInterface::Plugin *plugin = *it;
-    if ( !activeSummaries.contains( plugin->identifier() ) ) {
-      continue;
-    }
+    //winterz: comment out the next block because I think showing the
+    //configuration page for all summaries, even if they aren't active,
+    //is less confusing for the user.
+    //if ( !activeSummaries.contains( plugin->identifier() ) ) {
+    //  continue;
+    //}
 
     KontactInterface::Summary *summary = plugin->createSummaryWidget( mFrame );
     if ( summary ) {

commit 56819ea2f695260301a5313b182d632501f77a40

don't display the summary plugin unless it is also active. fixes the bug where all summaries are shown. caused by r1030887 MERGE: none DO_NOT_BACKPORT:
svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=1031256

kontact/plugins/summary/summaryview_part.cpp
@@ -178,7 +178,9 @@ void SummaryViewPart::updateWidgets()
           mLeftColumnSummaries.append( plugin->identifier() );
         }
 
-        loadedSummaries.append( plugin->identifier() );
+        if ( activeSummaries.contains( plugin->identifier() ) ) {
+          loadedSummaries.append( plugin->identifier() );
+        }
       } else {
         summary->hide();
       }