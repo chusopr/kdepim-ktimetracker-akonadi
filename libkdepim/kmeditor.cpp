chusopr/kdepim-ktimetracker-akonadi

commit 39b0bf319224f480b1a7607e38e5317629581d88

always try to replace the old sig by the new one, and only append one if none was found.
svn path=/branches/work/akonadi-ports/kdepim/; revision=1058302

libkdepim/kmeditor.cpp
@@ -571,19 +571,21 @@ KMeditorPrivate::signaturePositions( const KPIMIdentities::Signature &sig ) cons
   return signaturePositions;
 }
 
-void KMeditor::replaceSignature( const KPIMIdentities::Signature &oldSig,
+bool KMeditor::replaceSignature( const KPIMIdentities::Signature &oldSig,
                                  const KPIMIdentities::Signature &newSig )
 {
+  bool found = false;
   QTextCursor cursor( document() );
   cursor.beginEditBlock();
 
   QString oldSigText = oldSig.plainText();
-
+  if ( oldSigText.isEmpty() )
+    return false;
   int currentSearchPosition = 0;
   forever {
 
     // Find the next occurrence of the signature text
-    QString text = document()->toPlainText();
+    const QString text = document()->toPlainText();
     int currentMatch = text.indexOf( oldSigText, currentSearchPosition );
     currentSearchPosition = currentMatch;
     if ( currentMatch == -1 ) {
@@ -616,11 +618,13 @@ void KMeditor::replaceSignature( const KPIMIdentities::Signature &oldSig,
     cursor.removeSelectedText();
     setTextCursor( cursor );
     newSig.insertIntoTextEdit( this, KPIMIdentities::Signature::AtCursor, false, false );
+    found = true;
 
     currentSearchPosition += newSig.plainText().length();
   }
 
   cursor.endEditBlock();
+  return found;
 }
 
 #include "kmeditor.moc"

kmail/kmcomposewin.cpp
@@ -3687,16 +3687,17 @@ void KMComposeWin::slotIdentityChanged( uint uoid, bool initalChange )
     msgCleared = true;
   }
 
-  if ( msgCleared || oldSig.rawText().isEmpty() ) {
-    // Just append the signature if there is no old signature
+  //replace existing signatures
+  const bool replaced = mEditor->replaceSignature( oldSig, newSig );
+
+  // Just append the signature if there was no old signature
+  if ( !replaced && ( msgCleared || oldSig.rawText().isEmpty() ) ) {
     if ( GlobalSettings::self()->autoTextSignature()=="auto" ) {
       if ( GlobalSettings::self()->prependSignature() )
         newSig.insertIntoTextEdit( mEditor, KPIMIdentities::Signature::Start, true );
       else
         newSig.insertIntoTextEdit( mEditor, KPIMIdentities::Signature::End, true );
     }
-  } else {
-    mEditor->replaceSignature( oldSig, newSig );
   }
 
   // disable certain actions if there is no PGP user identity set

libkdepim/kmeditor.h
@@ -142,8 +142,9 @@ class KDEPIM_EXPORT KMeditor : public KPIMTextEdit::TextEdit
      *
      * @param oldSig the old signature, which will be replaced
      * @param newSig the new signature
+     * @return @p true if oldSig was found (and replaced) at least once
      */
-    void replaceSignature( const KPIMIdentities::Signature &oldSig,
+     bool replaceSignature( const KPIMIdentities::Signature &oldSig,
                            const KPIMIdentities::Signature &newSig );
 
   public Q_SLOTS:

commit 3d912e34624b80a22a0d03567daac5cf18bf5cfe

Don't return with a beginEditBlock() and no matching endEditBlock(). This completely messes up the composer, the text was not shown and toPlainText() returned an empty string.
Regression introduced in r1058302.

BUG: 223458

svn path=/trunk/KDE/kdepim/; revision=1093966

libkdepim/kmeditor.cpp
@@ -575,12 +575,12 @@ bool KMeditor::replaceSignature( const KPIMIdentities::Signature &oldSig,
                                  const KPIMIdentities::Signature &newSig )
 {
   bool found = false;
-  QTextCursor cursor( document() );
-  cursor.beginEditBlock();
-
   QString oldSigText = oldSig.toPlainText();
   if ( oldSigText.isEmpty() )
     return false;
+
+  QTextCursor cursor( document() );
+  cursor.beginEditBlock();
   int currentSearchPosition = 0;
   forever {
 