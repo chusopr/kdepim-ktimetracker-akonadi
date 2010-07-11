chusopr/kdepim-ktimetracker-akonadi

commit d918ad5d34a25da63f17fb0b9a5470e72e214d39

Use a slider in stead of a combo for task progress.
The only issue I didn't fix yet is setting the slider position to the value
that was clicked in stead of moving it on step back or forth.

svn path=/trunk/KDE/kdepim/; revision=1148048

incidenceeditors/incidenceeditor-ng/incidencecompletionpriority.cpp
@@ -46,14 +46,17 @@ class IncidenceCompletionPriority::Private
     int mOrigPercentCompleted;
 
   public: // slots
-    void comboValueChanged();
+    void sliderValueChanged( int );
 };
 
-void IncidenceCompletionPriority::Private::comboValueChanged()
+void IncidenceCompletionPriority::Private::sliderValueChanged( int value )
 {
-  if ( q->sender() == mUi->mCompletionCombo ) {
+  if ( q->sender() == mUi->mCompletionSlider ) {
     mOrigPercentCompleted = -1;
   }
+
+  mUi->mCompletedLabel->setText( i18n( "%1% completed", value ) );
+
   mDirty = true;
   q->checkDirtyStatus();
 }
@@ -66,13 +69,14 @@ IncidenceCompletionPriority::IncidenceCompletionPriority( Ui::EventOrTodoDesktop
  
   d->mUi = ui;
 
+  d->sliderValueChanged( d->mUi->mCompletionSlider->value() );
   d->mUi->mCompletionPriorityWidget->hide();
   d->mUi->mTaskLabel->hide();
 #ifndef KDEPIM_MOBILE_UI
   d->mUi->mTaskSeparator->hide();
 #endif
 
-  connect( d->mUi->mCompletionCombo, SIGNAL( currentIndexChanged( int ) ), SLOT( comboValueChanged() ) );
+  connect( d->mUi->mCompletionSlider, SIGNAL( valueChanged( int ) ), SLOT( sliderValueChanged( int ) ) );
   connect( d->mUi->mPriorityCombo, SIGNAL( currentIndexChanged( int ) ), SLOT( comboValueChanged() ) );
 }
 
@@ -99,9 +103,10 @@ void IncidenceCompletionPriority::load( KCal::Incidence::ConstPtr incidence )
 #endif
 
   d->mOrigPercentCompleted = todo->percentComplete();
-  d->mUi->mCompletionCombo->blockSignals( true );
-  d->mUi->mCompletionCombo->setCurrentIndex( todo->percentComplete() / 10 );
-  d->mUi->mCompletionCombo->blockSignals( false );
+  d->mUi->mCompletionSlider->blockSignals( true );
+  d->mUi->mCompletionSlider->setValue( todo->percentComplete() );
+  d->sliderValueChanged( d->mUi->mCompletionSlider->value() );
+  d->mUi->mCompletionSlider->blockSignals( false );
   
   d->mUi->mPriorityCombo->blockSignals( true );
   d->mUi->mPriorityCombo->setCurrentIndex( todo->priority() );
@@ -125,7 +130,7 @@ void IncidenceCompletionPriority::save( KCal::Incidence::Ptr incidence )
   if ( d->mOrigPercentCompleted != -1 ) {
     todo->setPercentComplete( d->mOrigPercentCompleted );
   } else {
-    todo->setPercentComplete( d->mUi->mCompletionCombo->currentIndex() * 10 );
+    todo->setPercentComplete( d->mUi->mCompletionSlider->value() );
   }
   todo->setPriority( d->mUi->mPriorityCombo->currentIndex() );
 

incidenceeditors/incidenceeditor-ng/incidencecompletionpriority.h
@@ -44,7 +44,7 @@ class INCIDENCEEDITORS_NG_EXPORT IncidenceCompletionPriority : public IncidenceE
     class Private;
     Private *const d;
 
-  Q_PRIVATE_SLOT( d, void comboValueChanged() )
+  Q_PRIVATE_SLOT( d, void sliderValueChanged(int) )
 };
 
 }

incidenceeditors/incidenceeditor-ng/eventortododesktop.ui
@@ -1314,83 +1314,39 @@
     <widget class="QWidget" name="mCompletionPriorityWidget" native="true">
      <layout class="QHBoxLayout" name="horizontalLayout_8">
       <item>
-       <widget class="QComboBox" name="mCompletionCombo">
-        <item>
-         <property name="text">
-          <string>0 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>10 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>20 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>30 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>40 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>50 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>60 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>70 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>80 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>90 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>100 %</string>
-         </property>
-        </item>
+       <widget class="QSlider" name="mCompletionSlider">
+        <property name="maximum">
+         <number>100</number>
+        </property>
+        <property name="singleStep">
+         <number>10</number>
+        </property>
+        <property name="tracking">
+         <bool>true</bool>
+        </property>
+        <property name="orientation">
+         <enum>Qt::Horizontal</enum>
+        </property>
+        <property name="tickPosition">
+         <enum>QSlider::TicksBelow</enum>
+        </property>
+        <property name="tickInterval">
+         <number>10</number>
+        </property>
        </widget>
       </item>
       <item>
-       <widget class="QLabel" name="label_7">
+       <widget class="QLabel" name="mCompletedLabel">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="MinimumExpanding" vsizetype="Preferred">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
         <property name="text">
          <string>completed</string>
         </property>
        </widget>
-      </item>
-      <item>
-       <spacer name="horizontalSpacer_13">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>329</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
       </item>
       <item>
        <widget class="QLabel" name="label_9">

incidenceeditors/incidenceeditor-ng/eventortodomobile.ui
@@ -255,66 +255,20 @@
        <number>0</number>
       </property>
       <item>
-       <widget class="QComboBox" name="mCompletionCombo">
-        <item>
-         <property name="text">
-          <string>0 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>10 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>20 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>30 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>40 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>50 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>60 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>70 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>80 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>90 %</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>100 %</string>
-         </property>
-        </item>
+       <widget class="QSlider" name="mCompletionSlider">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+        <property name="orientation">
+         <enum>Qt::Horizontal</enum>
+        </property>
        </widget>
       </item>
       <item>
-       <widget class="QLabel" name="label_7">
+       <widget class="QLabel" name="mCompletedLabel">
         <property name="text">
          <string>completed</string>
         </property>
@@ -325,9 +279,12 @@
         <property name="orientation">
          <enum>Qt::Horizontal</enum>
         </property>
+        <property name="sizeType">
+         <enum>QSizePolicy::MinimumExpanding</enum>
+        </property>
         <property name="sizeHint" stdset="0">
          <size>
-          <width>40</width>
+          <width>20</width>
           <height>20</height>
          </size>
         </property>

commit 73173254dd2db48d60df37747f0e5e6a2f98cba4

Fix regression caused by commit r1148048
the slot hijacked by the priority combo to set the dirty flag was removed, so
add a new slot to set the dirty flag.

svn path=/trunk/KDE/kdepim/; revision=1148709

incidenceeditors/incidenceeditor-ng/incidencecompletionpriority.cpp
@@ -47,6 +47,7 @@ class IncidenceCompletionPriority::Private
 
   public: // slots
     void sliderValueChanged( int );
+    void slotSetDirty();
 };
 
 void IncidenceCompletionPriority::Private::sliderValueChanged( int value )
@@ -57,9 +58,15 @@ void IncidenceCompletionPriority::Private::sliderValueChanged( int value )
 
   mUi->mCompletedLabel->setText( i18n( "%1% completed", value ) );
 
-  mDirty = true;
-  q->checkDirtyStatus();
+  slotSetDirty();
 }
+
+void IncidenceCompletionPriority::Private::slotSetDirty()
+{
+    mDirty = true;
+    q->checkDirtyStatus();
+}
+
 
 IncidenceCompletionPriority::IncidenceCompletionPriority( Ui::EventOrTodoDesktop *ui )
   : IncidenceEditor()
@@ -77,7 +84,7 @@ IncidenceCompletionPriority::IncidenceCompletionPriority( Ui::EventOrTodoDesktop
 #endif
 
   connect( d->mUi->mCompletionSlider, SIGNAL( valueChanged( int ) ), SLOT( sliderValueChanged( int ) ) );
-  connect( d->mUi->mPriorityCombo, SIGNAL( currentIndexChanged( int ) ), SLOT( comboValueChanged() ) );
+  connect( d->mUi->mPriorityCombo, SIGNAL( currentIndexChanged( int ) ), SLOT( slotSetDirty() ) );
 }
 
 IncidenceCompletionPriority::~IncidenceCompletionPriority()

incidenceeditors/incidenceeditor-ng/incidencecompletionpriority.h
@@ -45,6 +45,7 @@ class INCIDENCEEDITORS_NG_EXPORT IncidenceCompletionPriority : public IncidenceE
     Private *const d;
 
   Q_PRIVATE_SLOT( d, void sliderValueChanged(int) )
+  Q_PRIVATE_SLOT( d, void slotSetDirty() )
 };
 
 }